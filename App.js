d36f2123f40604af6e76f344b5127cfba573218c
Rahul Gupta <contact.rahul1660@gmail.com>
2026-02-05 20:27:05 +0530

    changes
    changes

--- App.js
+++ App.js
@@ -1,9 +1,32 @@
+/*
+ *
+ * <div id="parent">
+ *   <div class="child">
+ *     <h1>Hello</h1>
+ *   </div>
+ * </div>
+ *
+ */
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { className: "child" },
+    React.createElement("h1", {}, "hello"),
+  ),
+);
+
 const h1 = React.createElement(
   "h1",
   { id: "heading", className: "head med" },
   "Hello world from react",
 );
-console.log(h1);
+console.log(parent);
+console.log(h1); //object
 console.log(root);
-root.render(h1);
+// root.render(h1);
+root.render(parent);
